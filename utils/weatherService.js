d5066b67b3376d16fed34a02de3c1c2ddc76d943
muskan02062000 <sharmamuskan262000@gmail.com>
2025-02-03 16:04:19 +0530

    Update weatherService.js

--- utils/weatherService.js
+++ utils/weatherService.js
@@ -1,1 +1,23 @@
 
+const axios = require("axios");
+ 
+const fetchWeather = async (location, date) => {
+    try {
+const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+            params: {
+                q: location,
+                appid: process.env.WEATHER_API_KEY,
+                units: "metric"
+            }
+        });
+        return {
+            temperature: response.data.main.temp,
+            weather: response.data.weather[0].description
+        };
+    } catch (error) {
+        console.error("Weather API Error:", error);
+        return null;
+    }
+};
+ 
+module.exports = fetchWeather;
