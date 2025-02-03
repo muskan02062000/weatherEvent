df3f1a829248923ea1c734201baadaa9b1dde980
muskan02062000 <sharmamuskan262000@gmail.com>
2025-02-03 16:03:18 +0530

    Update Event.js

--- models/Event.js
+++ models/Event.js
@@ -1,1 +1,13 @@
-
+const mongoose = require("mongoose");
+ 
+const EventSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    location: { type: String, required: true },
+    weather: { type: Object }  // Store fetched weather data
+});
+ 
+module.exports = mongoose.model("Event", EventSchema);
